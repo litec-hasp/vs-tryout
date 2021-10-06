3687f00be9c429e37db1d02e305626bb2e176f6a
Peter Haslinger <hasp@litec.ac.at>
2021-10-06 23:08:43 +0200

    Improved main method in program.cs

--- vs-tryout/Program.cs
+++ vs-tryout/Program.cs
@@ -6,7 +6,7 @@ namespace vs_tryout
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GIT!");
         }
     }
 }
